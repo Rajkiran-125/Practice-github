c7f322e600eee6777adde2d8f3014449788f6421
Rajkiran Jaiswar <rajkiranj@smartyz.com>
2026-05-01 20:26:32 +0530

    New File test.txt has created

---
+++ app.js
@@ -0,0 +1,13 @@
+const sum = require('./main.js');
+
+// import {sum} from './main.js';
+
+await sum(1,2);
+var x = 0;
+
+module.exports.a = 10;
+
+console.log(__filename);
+
+
+console.log(globalThis === global); 
---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Inside the main File....');
+
+async function sum(a,b){
+    console.log(a+b);
+}
+
+module.exports = sum;
+
+console.log('git')
